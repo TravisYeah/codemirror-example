06065a94efb9cb04d6ee7021a1c39b42ee7fa311
Travis Harrison <travisharrisonyeah@gmail.com>
2024-11-27 08:44:53 -0600

    minimial imports

--- editor.mjs
+++ editor.mjs
@@ -1,9 +1,45 @@
-import { basicSetup, EditorView } from "codemirror";
+import { EditorView } from "codemirror";
 import { cql } from "codemirror-lang-cql";
-import { syntaxHighlighting } from "@codemirror/language";
+import { syntaxHighlighting, bracketMatching } from "@codemirror/language";
 import { highlightStyle } from "./theme.mjs";
+import {
+  autocompletion,
+  completionKeymap,
+  closeBrackets,
+  closeBracketsKeymap,
+} from "@codemirror/autocomplete";
+import { keymap } from "@codemirror/view";
+import { defaultKeymap, history, historyKeymap } from "@codemirror/commands";
+
+const theme = EditorView.theme({
+  "&": {
+    border: "1px solid darkgrey",
+    "border-radius": "10px",
+  },
+  "&.cm-editor.cm-focused": {
+    outline: "none",
+  },
+  "&.cm-editor.cm-line": {
+    "line-height": "30px",
+  },
+});
 
 new EditorView({
-  extensions: [basicSetup, syntaxHighlighting(highlightStyle), cql()],
+  extensions: [
+    //basicSetup,
+    cql(),
+    syntaxHighlighting(highlightStyle),
+    autocompletion(),
+    closeBrackets(),
+    bracketMatching(),
+    history(),
+    keymap.of([
+      ...closeBracketsKeymap,
+      ...completionKeymap,
+      ...defaultKeymap,
+      ...historyKeymap,
+    ]),
+    theme,
+  ],
   parent: document.body,
 });
